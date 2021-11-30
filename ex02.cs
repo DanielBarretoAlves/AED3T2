04b1970d1f0e2f9d00b889db736b058d8597f624
Daniel Barreto Alves <danielbarreto559@gmail.com>
2021-11-29 21:23:39 -0300

    Added Num List

---
+++ ex02.cs
@@ -0,0 +1,24 @@
+class ex02
+{
+    public void resposta()
+    {
+        int[] numList = new int[16];
+        numList[0] = 3;
+        numList[1] = 4;
+        numList[2] = 9;
+        numList[3] = 2;
+        numList[4] = 5;
+        numList[5] = 8;
+        numList[6] = 2;
+        numList[7] = 1;
+        numList[8] = 7;
+        numList[9] = 4;
+        numList[10] = 6;
+        numList[11] = 2;
+        numList[12] = 9;
+        numList[13] = 8;
+        numList[14] = 5;
+        numList[15] = 1;
+
+    }
+}
