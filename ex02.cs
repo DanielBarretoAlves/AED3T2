f4f60ca7330e6a4bf7a7cd69f6a6484be3a44e6a
DanielBarretoAlves <DanielBarretoAlves>
2021-12-07 18:09:20 -0300

    Ex02 COMPLETED

--- ex02.cs
+++ ex02.cs
@@ -19,34 +19,169 @@ class ex02
         numList[13] = 8;
         numList[14] = 5;
         numList[15] = 1;
-
-    }
-
-    private static void MergeSortNumbers(int[] B, int iStart, int iEnd, int[] A)
-    {
-        if ((iEnd - iStart) < 2) return;
-
-        int iMiddle = (iStart + iEnd) / 2;
-        MergeSortNumbers(A, iStart, iMiddle, B); // Do Not decrement iMiddle
-        MergeSortNumbers(A, iMiddle, iEnd, B);
-        Merge(B, iStart, iMiddle, iEnd, A);
-    }
-
-    private static void Merge(int[] A, int iStart, int iMiddle, int iEnd, int[] B)
-    {
-        int i = iStart;
-        int j = iMiddle;
-
-        for (int k = iStart; k < iEnd; k++)
+        int[] testQuick = quickSort(numList);
+        int[] testHeap = numList;
+        int[] testMerge = numList;
+        testMerge = MergeSort(testMerge, 0 , testMerge.Length - 1);
+        testHeap = heapSort(testHeap, testHeap.Length);
+        Console.WriteLine("Merge Sort:");
+        for (int index = 0; index < testQuick.Length; index++)
         {
-            if (i < iMiddle && (j >= iEnd || A[i] <= A[j]))
+            Console.Write(testMerge[index] + ", ");
+            
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Heap Sort:");
+        
+
+        for (int index = 0; index < testQuick.Length; index++)
+        {
+            Console.Write(testHeap[index] + ", ");
+            
+        }
+        Console.WriteLine("");
+        Console.WriteLine("Quick Sort:");
+        for (int index = 0; index < testQuick.Length; index++)
+        {
+            Console.Write(testQuick[index] + ", ");
+            
+        }
+
+    }
+
+    //---------------------------------QUICK SORT-------------------------------------------------------------
+
+    public static int[] quickSort(int[] vetor)
+    {
+        int inicio = 0;
+        int fim = vetor.Length - 1;
+
+        quickSort(vetor, inicio, fim);
+
+        return vetor;
+    }
+
+    private static void quickSort(int[] vetor, int inicio, int fim)
+    {
+        if (inicio < fim)
+        {
+            int p = vetor[inicio];
+            int i = inicio + 1;
+            int f = fim;
+
+            while (i <= f)
             {
-                B[k] = A[i++];
+                if (vetor[i] <= p)
+                {
+                    i++;
+                }
+                else if (p < vetor[f])
+                {
+                    f--;
+                }
+                else
+                {
+                    int troca = vetor[i];
+                    vetor[i] = vetor[f];
+                    vetor[f] = troca;
+                    i++;
+                    f--;
+                }
+            }
+
+            vetor[inicio] = vetor[f];
+            vetor[f] = p;
+
+            quickSort(vetor, inicio, f - 1);
+            quickSort(vetor, f + 1, fim);
+
+
+        }
+    }
+    //-----------------------------------------------------------------------------------------------------
+    //-------------------------------------------HEAP SORT----------------------------------------------------------
+
+    static int[] heapSort(int[] arr, int n)
+    {
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+        return arr;
+    }
+    static void heapify(int[] arr, int n, int i)
+    {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------
+    //---------------------------------------MERGE SORT--------------------------------------------------------------
+    private void Merge(int[] input, int left, int middle, int right)
+    {
+        int[] leftArray = new int[middle - left + 1];
+        int[] rightArray = new int[right - middle];
+
+        Array.Copy(input, left, leftArray, 0, middle - left + 1);
+        Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+        int i = 0;
+        int j = 0;
+        for (int k = left; k < right + 1; k++)
+        {
+            if (i == leftArray.Length)
+            {
+                input[k] = rightArray[j];
+                j++;
+            }
+            else if (j == rightArray.Length)
+            {
+                input[k] = leftArray[i];
+                i++;
+            }
+            else if (leftArray[i] <= rightArray[j])
+            {
+                input[k] = leftArray[i];
+                i++;
             }
             else
             {
-                B[k] = A[j++];
+                input[k] = rightArray[j];
+                j++;
             }
         }
+       
     }
+
+    private int[] MergeSort(int[] input, int left, int right)
+    {
+        if (left < right)
+        {
+            int middle = (left + right) / 2;
+
+            MergeSort(input, left, middle);
+            MergeSort(input, middle + 1, right);
+
+            Merge(input, left, middle, right);
+        }
+        return input;
+    }
+    //-----------------------------------------------------------------------------------------------------
 }
