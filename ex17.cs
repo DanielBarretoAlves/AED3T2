378900f7a47e187743234764d0450f3139754d2b
DanielBarretoAlves <DanielBarretoAlves>
2021-12-07 19:11:07 -0300

    ex17

---
+++ ex17.cs
@@ -0,0 +1,7 @@
+class ex17
+{
+    public void resposta()
+    {
+        Console.WriteLine("Resposta 17: Devemos definir os incrmenetospois pois quanto maior o numero primo, melhor a cripitografia" );
+    }
+}
