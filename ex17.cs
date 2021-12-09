d13ac92fe2616eddf842745509be5f7ad5b1028b
Maqui15 <matheusquintanilha@ucl.br>
2021-12-09 14:05:41 -0300

    17 corrigida

--- ex17.cs
+++ ex17.cs
@@ -2,6 +2,6 @@ class ex17
 {
     public void resposta()
     {
-        Console.WriteLine("Resposta 17: Devemos definir os incrmenetospois pois quanto maior o numero primo, melhor a cripitografia" );
+        Console.WriteLine("Resposta 17: é importante para resolução de problemas com busca sequencial e colocar os numeros até a possição correta principal forma de utilização do Shellshort pois ele faz as trocas de posições mas tiretamente não importando a posição e a distancia, pois a troca é feita de forma direta. Porém infelizmente mesmo assim não é muito preciso pois nem sempre ele colocar os elementos repetidos em suas defidas posições. Com isso é necessário realizar as escolhas dos incrementos com primos entre si por conta das comparações que são realizadas para ocerrerem as trocas de possições nos elementos" );
     }
 }
